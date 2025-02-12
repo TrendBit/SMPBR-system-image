85398a9139339ae8789b0df5869036c67a1e6508
Petr Malanik <Malanik.Petr.gm@gmail.com>
2025-02-12 08:07:39 +0100

    flash: add switch to enable fetching latest firmware from GitHub

--- update_firmware.sh
+++ update_firmware.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 usage() {
-    echo "Usage: $0 [-l local_firmware_path]"
-    echo "  -l : Path to local firmware zip file (optional)"
-    echo "  If no local path specified, downloads latest firmware from GitHub"
+    echo "Usage: $0 [-l local_firmware_path] [-f]"
+    echo "  -l : Path to local firmware zip file"
+    echo "  -f : Fetch latest firmware from GitHub"
+    echo "  -h : Show this help"
     exit 1
 }
 
@@ -12,13 +13,21 @@ firmware_dir="./firmware"
 
 # Parse command line arguments
 local_firmware=""
-while getopts "l:h" opt; do
+fetch_firmware=false
+while getopts "l:fh" opt; do
     case $opt in
         l) local_firmware="$OPTARG" ;;
+        f) fetch_firmware=true ;;
         h) usage ;;
         ?) usage ;;
     esac
 done
+
+# Ensure at least one option is specified
+if [ -z "$local_firmware" ] && [ "$fetch_firmware" = false ]; then
+    echo "Warning: No action specified"
+    usage
+fi
 
 # Ensure the firmware directory exists
 mkdir -p "$firmware_dir"
@@ -31,7 +40,7 @@ if [ -n "$local_firmware" ]; then
     echo "Using local firmware: $local_firmware"
     cp "$local_firmware" "$firmware_dir/$(basename "$local_firmware")"
     archive_name="$(basename "$local_firmware")"
-else
+elif [ "$fetch_firmware" = true ]; then
     # Fetch the latest release URL
     url=$(curl -s https://api.github.com/repos/TrendBit/SMBR-firmware/releases/latest | grep "browser_download_url.*\.zip" | cut -d '"' -f 4)
 
