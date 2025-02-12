545607c4ccaf610cf602a3a1dadb2672c6fab8ad
Petr Malanik <Malanik.Petr.gm@gmail.com>
2025-02-12 08:08:19 +0100

    update: add option to reset repository (by --reset switch)

--- update_services.sh
+++ update_services.sh
@@ -1,11 +1,46 @@
 #!/bin/bash
+
+# Parse arguments
+RESET=false
+
+usage() {
+    echo "Usage: $0 [-r|--reset]"
+    echo "Options:"
+    echo "  -r, --reset    Reset local repository changes and clean untracked files"
+    exit 1
+}
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -r|--reset)
+            RESET=true
+            shift
+            ;;
+        -h|--help)
+            usage
+            ;;
+        *)
+            echo "Unknown parameter: $1"
+            usage
+            ;;
+    esac
+done
+
+reset_repo() {
+    if [ "$RESET" = true ]; then
+        git reset --hard
+        git clean -fd
+    fi
+}
+
 sudo systemctl stop api-server
 sudo systemctl stop core-module
 sudo systemctl stop telegraf
-RECIPE_RUNNER_ACTIVE=$(systemctl is-active recipe-runner)
+RECIPE_RUNNER_ACTIVE=$(sudo systemctl is-active recipe-runner)
 sudo systemctl stop recipe-runner
 
 cd /home/reactor/api-server
+reset_repo
 git pull
 cd build
 cmake ..
@@ -14,6 +49,7 @@ sudo make install
 sudo make install-service
 
 cd /home/reactor/core-module
+reset_repo
 git pull
 cd build
 cmake ..
@@ -22,6 +58,7 @@ sudo make install
 sudo make install-service
 
 cd /home/reactor/recipe-runner
+reset_repo
 git pull
 cd build
 cmake ..
@@ -30,6 +67,7 @@ sudo make install
 sudo make install-service
 
 cd /home/reactor/database-export
+reset_repo
 git pull
 sudo cp /home/reactor/database-export/telegraf.service /etc/systemd/system/
 
